ef1807980b001ad41f4340a9d8f5b7452450abb8
mario <mario.saliba98@gmail.com>
2018-02-26 19:04:52 +0200

    init elm code rel #1

---
+++ src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Card =
+    { id : String
+    , img : String
+    , matchFound : Bool
+    }
+
+
+type alias Model =
+    { cards : List Card
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    Model
+        [ { id = "finn", img = "https://avatars3.githubusercontent.com/u/14337958?v=4", matchFound = False }
+        , { id = "aisha", img = "https://avatars3.githubusercontent.com/u/22300773?v=4", matchFound = False }
+        , { id = "suha", img = "https://avatars1.githubusercontent.com/u/24496866?v=4", matchFound = False }
+        ]
+        ! []
+
+
+type Msg
+    = NoOp
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] (List.map renderCard model.cards)
+        ]
+
+
+renderCard : Card -> Html Msg
+renderCard card =
+    div []
+        [ img [ src card.img ] []
+        ]
