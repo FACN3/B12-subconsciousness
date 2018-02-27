a83364f814bd1f18d03cc0a799af5e55e1a885d2
MynahMarie <mynah.marie@gmail.com>
2018-02-27 12:28:13 +0200

    Starting to implement the show/hide functions of the cards Relates #9

--- src/Main.elm
+++ src/Main.elm
@@ -22,25 +22,27 @@ type alias Card =
     { id : String
     , img : String
     , matchFound : Bool
+    , flipped : Bool
     }
 
 
 type alias Model =
     { cards : List Card
     , backImage : String
+    , hideBtn : Bool
     }
 
 
 cards : List Card
 cards =
-    [ { id = "finn", img = "https://avatars3.githubusercontent.com/u/14337958?v=4", matchFound = False }
-    , { id = "aisha", img = "https://avatars3.githubusercontent.com/u/22300773?v=4", matchFound = False }
-    , { id = "suha", img = "https://avatars1.githubusercontent.com/u/24496866?v=4", matchFound = False }
-    , { id = "james-blonde", img = "https://avatars0.githubusercontent.com/u/25667270?v=4", matchFound = False }
-    , { id = "shireen", img = "https://avatars3.githubusercontent.com/u/22002193?v=4", matchFound = False }
-    , { id = "king", img = "https://avatars3.githubusercontent.com/u/25408167?v=4", matchFound = False }
-    , { id = "des-des", img = "https://avatars1.githubusercontent.com/u/12845233?v=4", matchFound = False }
-    , { id = "claire", img = "https://avatars2.githubusercontent.com/u/10425219?v=4", matchFound = False }
+    [ { id = "finn", img = "https://avatars3.githubusercontent.com/u/14337958?v=4", matchFound = False, flipped = False }
+    , { id = "aisha", img = "https://avatars3.githubusercontent.com/u/22300773?v=4", matchFound = False, flipped = False }
+    , { id = "suha", img = "https://avatars1.githubusercontent.com/u/24496866?v=4", matchFound = False, flipped = False }
+    , { id = "james-blonde", img = "https://avatars0.githubusercontent.com/u/25667270?v=4", matchFound = False, flipped = False }
+    , { id = "shireen", img = "https://avatars3.githubusercontent.com/u/22002193?v=4", matchFound = False, flipped = False }
+    , { id = "king", img = "https://avatars3.githubusercontent.com/u/25408167?v=4", matchFound = False, flipped = False }
+    , { id = "des-des", img = "https://avatars1.githubusercontent.com/u/12845233?v=4", matchFound = False, flipped = False }
+    , { id = "claire", img = "https://avatars2.githubusercontent.com/u/10425219?v=4", matchFound = False, flipped = False }
     ]
 
 
@@ -49,6 +51,7 @@ init =
     Model
         (interweave cards cards)
         "https://theanalyticalcouchpotato.files.wordpress.com/2012/04/mtg-card-back1.jpg"
+        False
         ! []
 
 
@@ -70,7 +73,7 @@ update msg model =
             ( model, generate ShuffledDeck (shuffle model.cards) )
 
         ShuffledDeck shuffledDeck ->
-            { model | cards = shuffledDeck } ! []
+            { model | cards = List.map (\x -> { x | flipped = True }) shuffledDeck, hideBtn = True } ! []
 
         NoOp ->
             model ! []
@@ -79,15 +82,22 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ div [ class "flex items-center justify-center pv4" ]
-            [ button [ onClick ShuffleCards, class "" ] [ text "New Game" ]
+        [ div [ class "flex items-center justify-center" ]
+            [ button [ hidden model.hideBtn, onClick ShuffleCards, class "mv4" ] [ text "New Game" ]
             ]
-        , div [ class "flex w-100 justify-center flex-wrap ph7" ] (model.cards |> List.map renderCard)
+        , div [ class "flex w-100 justify-center items-center flex-wrap ph7 pv2" ] (model.cards |> List.map renderCard)
         ]
 
 
 renderCard : Card -> Html Msg
 renderCard card =
-    div [ class "w-25 pv3" ]
-        [ img [ src card.img, class "h4" ] []
-        ]
+    let
+        cardUrl =
+            if card.flipped then
+                card.img
+            else
+                "https://d1u5p3l4wpay3k.cloudfront.net/mtgsalvation_gamepedia/7/7a/Magic_card_back_2.jpg?version=6d697ae1a0e6361ac10505af3c75387a"
+    in
+        div [ class "w-25 pv3 tc" ]
+            [ img [ src cardUrl, class "h4 w4 br3" ] []
+            ]
